65028c740e3f8f83f1300a0e71839f6b3131f2a9
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-05-14 19:24:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,5 @@ class Config(object):
       API_ID = int(getenv("API_ID", 0))
       AS_COPY = True if getenv("AS_COPY", False) == "True" else False
       BOT_TOKEN = getenv("BOT_TOKEN", "")
-      CHANNEL = list(x for x in getenv("CHANNEL_ID", "").replace("\n", " ").split(':'))
+      FROM_CHANNEL = getenv("FROM_CHANNEL", "")
+      TO_CHANNEL = getenv("TO_CHANNEL", "")
