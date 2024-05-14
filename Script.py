48d81ee69a096e6784ef7268b638b006fa80ff84
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-05-14 20:29:28 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,8 +1,8 @@
 class script(object):
-    START = """Hi I am a channel auto forward bot clone of <a href='https://t.me/Autoforward_Nsbot'>Channel Automatic NS bot</a>. \nMake your own bot now 👉 Source code"""
+    START = """Hello, I am an automatic channel forwarding bot."""
     
     ABOUT = """
 <b>📝 Language:</b> Python 3
 <b>🧰 Framework:</b> Pyrogram
-<b>👨‍💻 Developer:</b> 𝐀𝐧𝐨𝐧𝐲𝐦𝐨𝐮𝐬
-<b>🌐Source Code:</b> Press Me 🥰"""
+<b>😎 Developer:</b> 𝐀𝐧𝐨𝐧𝐲𝐦𝐨𝐮𝐬
+<b>🌐 Source Code:</b> Privet 🥰"""
