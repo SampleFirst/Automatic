22308e638c53316c8937488209af7d6d7512f471
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-05-14 19:54:42 +0530

    Update and rename translation.py to Script.py

---
+++ Script.py
@@ -0,0 +1,8 @@
+class script(object):
+    START = """Hi I am a channel auto forward bot clone of <a href='https://t.me/Autoforward_Nsbot'>Channel Automatic NS bot</a>. \nMake your own bot now 👉 Source code"""
+    
+    ABOUT = """
+<b>📝 Language:</b> Python 3
+<b>🧰 Framework:</b> Pyrogram
+<b>👨‍💻 Developer:</b> 𝐀𝐧𝐨𝐧𝐲𝐦𝐨𝐮𝐬
+<b>🌐Source Code:</b> Press Me 🥰"""
